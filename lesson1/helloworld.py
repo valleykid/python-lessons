a7958daacd145e68e599f52ed19df4e03baaeb90
valleykid <valleykiddy@gmail.com>
2018-01-15 18:11:23 +0800

    Test print & class

---
+++ lesson1/helloworld.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python    // !/usr/bin/python
+# coding: utf-8         // =
+
+# print 'Hello World!';
+print '你好，世界!';
+
+# 第一个注释
+print "Hello, Python!";  # 第二个注释
+
+'''
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+'''
+
+"""
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+"""
+
+paragraph = """这是一个段落。
+包含了多个语句""";
+
+print paragraph;
